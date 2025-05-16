9059f06ba0d7dfc2067f7472001d91ecc8ea8ded
SerenKoca <r0937023@student.thomasmore.be>
2025-05-16 18:29:12 +0200

    added: tutorial and map

--- script.js
+++ script.js
@@ -64,9 +64,9 @@ updateZoomButtons();
 
 //---------------Echter streetview--------------------
   // Voeg de OpenStreetMap tegellaag toe
-  /*L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(map);*/
+  }).addTo(map);
 
 //---------------Onze kaart--------------------
   var imageUrl = 'images/kaart.svg';  
